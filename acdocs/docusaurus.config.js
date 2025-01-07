57ef4b7f19d747158fefa2bec8a28531382fc30d
Justin Pihony <justin.pihony@gmail.com>
2025-01-07 13:11:02 -0500

    Throw to warn for now

--- acdocs/docusaurus.config.js
+++ acdocs/docusaurus.config.js
@@ -27,7 +27,7 @@ const config = {
   organizationName: 'akka-cache', // Usually your GitHub org/user name.
   projectName: 'akka-cache', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
